0885e30e8b055c08ff5784de61c5f486716b5b75
Robert Herdzik <robert.herdzik@yahoo.com>
2018-02-07 17:26:54 +0100

    code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RHPlaceholder
 Simple code snippet which gives you ability to support 'loading' state for your VC
+
+
+
+---
+##TODO:
+- [ ] add shimmering animation
+- [ ] create pod
+- [ ] add color configuration
+- [ ] update readme
+
--- RHPlaceholder/ViewController.swift
+++ RHPlaceholder/ViewController.swift
@@ -2,6 +2,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var photoView1: UIView!
     @IBOutlet weak var photoView2: UIView!
     @IBOutlet weak var photoView3: UIView!
@@ -22,39 +23,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        roundImageContainers()
+        roundProfileImageContainers()
+        
+        // Adding placeholder 
         addPlaceholder()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
-            self.remove()
+        // Fetched data from API simulation
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.removePhaceholder()
         }
     }
     
     private func addPlaceholder() {
-        let labels: [UIView] = [photoImgView4!,
-                                numberOfMiles!,
-                                name!,
-                                surname!,
-                                age!,
-                                email!,
-                                birthDate!,
-                                sex!]
-        placeholderMarker.register(placeholders: labels)
+        let viewElements: [UIView] = [photoImgView4,
+                                      numberOfMiles,
+                                      name,
+                                      surname,
+                                      age,
+                                      email,
+                                      birthDate,
+                                      sex]
+        placeholderMarker.register(viewElements)
     }
 
-    @objc private func remove() {
-        placeholderMarker.removeLayer()
+    @objc private func removePhaceholder() {
+        placeholderMarker.remove()
     }
     
-    private func roundImageContainers() {
-        photoView1.layer.cornerRadius = photoView1.bounds.width/2
-        photoView2.layer.cornerRadius = photoView2.bounds.width/2
-        photoView3.layer.cornerRadius = photoView3.bounds.width/2
-        photoImgView4.layer.cornerRadius = photoImgView4.bounds.width/2
+    private func roundProfileImageContainers() {
+        [photoView1, photoView2, photoView3, photoImgView4].forEach {
+            $0.layer.cornerRadius = $0.bounds.width/2
+        }
         photoImgView4.layer.masksToBounds = true
     }
 }
@@ -62,54 +65,81 @@ class ViewController: UIViewController {
 class RHPlaceholder {
     
     private var placeholders = [RHPlaceholderItem]()
+    private var layerAnimator: RHLayerAnimating
     
-    func register(placeholders: [UIView]) {
-        placeholders.forEach { label in
-            self.placeholders.append(RHPlaceholderItem(originItem: label,
-                                                       cover: UIView()))
+    init(layerAnimator: RHLayerAnimating) {
+        self.layerAnimator = layerAnimator
+    }
+    
+    convenience init() {
+        self.init(layerAnimator: RHLayerAnimatorDefault())
+    }
+    
+    func register(_ viewElements: [UIView]) {
+        viewElements.forEach {
+            let placeholderItem = RHPlaceholderItem(originItem: $0)
+            self.placeholders.append(placeholderItem)
         }
     
         addLayer()
     }
     
-    private func addLayer() {
-        placeholders.forEach({ placeholder in
-            let layer = placeholder.cover
-            layer.backgroundColor = UIColor.lightGray
-            layer.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            
-            layer.frame = placeholder.originItem.bounds
-            placeholder.originItem.addSubview(layer)
-        })
-        
-        startShimmering()
-    }
-    
-    func removeLayer() {
-        placeholders.forEach({ placeholder in
-            let layer = placeholder.cover
+    func remove() {
+        placeholders.forEach { placeholder in
+            let layer = placeholder.shield
             layer.removeFromSuperview()
-        })
+        }
     }
     
-    func startShimmering() {
-        placeholders.forEach({ placeholder in
-            let layer = placeholder.cover
-            
-            let animation = CABasicAnimation()
-            animation.keyPath = "backgroundColor"
-            animation.toValue = UIColor.gray.cgColor
-            animation.duration = 0.6
-            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-            
-            animation.repeatCount = Float.greatestFiniteMagnitude
-            layer.layer.add(animation, forKey: "bg")
-        })
+    private func addLayer() {
+        placeholders.forEach { placeholder in
+            addShieldViewOriginView(from: placeholder)
+        }
+        
+        animate()
+    }
+    
+    private func addShieldViewOriginView(from placeholder: RHPlaceholderItem) {
+        let shield = placeholder.shield
+        shield.backgroundColor = UIColor.lightGray
+        shield.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        shield.frame = placeholder.originItem.bounds
+        placeholder.originItem.addSubview(shield)
+    }
+    
+    private func animate() {
+        placeholders.forEach { [weak self] in
+            let layer = $0.shield.layer
+            self?.layerAnimator.addAnimation(to: layer)
+        }
+    }
+}
+
+protocol RHLayerAnimating {
+    func addAnimation(to layer: CALayer)
+}
+
+struct RHLayerAnimatorDefault: RHLayerAnimating {
+    
+    func addAnimation(to layer: CALayer) {
+        let animation = CABasicAnimation()
+        animation.keyPath = "backgroundColor"
+        animation.toValue = UIColor.gray.cgColor
+        animation.duration = 0.6
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.repeatCount = Float.greatestFiniteMagnitude
+        
+        layer.add(animation, forKey: "bg_animation")
     }
 }
 
 struct RHPlaceholderItem {
     
-    let originItem: UIView
-    var cover: UIView
+    let originItem: UIView // TODO [🌶]: consider 'weak'
+    let shield = UIView()
+    
+    init(originItem: UIView) {
+        self.originItem = originItem
+    }
 }
