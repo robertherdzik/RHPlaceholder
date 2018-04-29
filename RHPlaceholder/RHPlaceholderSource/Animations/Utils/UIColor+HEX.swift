d3e7e2e39567de673f46e046a541cbc2c6c85c0c
Robert Herdzik <robert.herdzik@yahoo.com>
2018-04-29 11:17:47 +0200

    Generating color based on hex

---
+++ RHPlaceholder/RHPlaceholderSource/Animations/Utils/UIColor+HEX.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        let decimalRange = 0...255
+        assert(decimalRange ~= red, "Invalid red component")
+        assert(decimalRange ~= green, "Invalid green component")
+        assert(decimalRange ~= blue, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0,
+                  green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0,
+                  alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
