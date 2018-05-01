031ff3ac08654f920538cf4a363897e366555188
Robert Herdzik <robert.herdzik@yahoo.com>
2018-05-01 12:02:33 +0200

    Readme update

--- RHPlaceholder/ViewController.swift
+++ RHPlaceholder/ViewController.swift
@@ -39,6 +39,8 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    
     private func addPlaceholder() {
         let viewElements: [UIView] = [
             photoImgView4,
