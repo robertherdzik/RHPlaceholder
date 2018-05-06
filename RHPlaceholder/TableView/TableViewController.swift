d5d190bb6fd57e4e398c31d4e3805974623a5a06
Robert Herdzik <robert.herdzik@yahoo.com>
2018-05-06 12:02:48 +0200

    Tb added to Storyboard

---
+++ RHPlaceholder/TableView/TableViewController.swift
@@ -0,0 +1,40 @@
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    private var dataLoaded = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.dataLoaded = true
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100 // TODO [🌶]: get height from cell
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! MyCell
+        
+//        cell.imageView?.backgroundColor = UIColor.green
+//        cell.dataLoaded = dataLoaded
+        
+        return cell
+    }
+}
